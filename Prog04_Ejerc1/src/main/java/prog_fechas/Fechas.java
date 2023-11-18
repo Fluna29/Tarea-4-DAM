45379bb6a098478b4c9900f658a795fba225a2f5
Fluna29 <fluna293107@gmail.com>
2023-11-18 12:57:38 +0100

    Clase fechas creada y finalizada

--- Prog04_Ejerc1/src/main/java/prog_fechas/Fechas.java
+++ Prog04_Ejerc1/src/main/java/prog_fechas/Fechas.java
@@ -4,9 +4,9 @@ package prog_fechas;
  *
  * @author fluna
  */
+
 public class Fechas {
-    public static void fechas (String[] args){
-        enum enumMes {
+    enum enumMes {
             ENERO, 
             FEBRERO, 
             MARZO,
@@ -14,11 +14,58 @@ public class Fechas {
             MAYO,
             JUNIO,
             JULIO,
-            AGOSTO,
+            AGOSTO, 
             SEPTIEMBRE,
             OCTUBRE,
             NOVIEMBRE,
             DICIEMBRE,
         }
+    private int dia;
+    private enumMes mes;
+    private int anio;
+    public void fechas(int dia, enumMes mes, int anio){
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+        
+        public int getDia() {
+            return dia;
+        }
+        public void setDia(int dia){
+            dia = this.dia;
+        }
+        public enumMes getMes(){
+            return mes;
+        }
+        public void setMes(enumMes mes){
+            this.mes = mes;
+        }
+        public int getAnio(){
+            return anio;
+        }
+        public void setAnio(int anio){
+            this.anio = anio;
+        }
+
+        public boolean isSummer(){
+            switch (getMes) {
+                case JUNIO:
+                    return true;
+                    break;
+                case JULIO:
+                    return true;
+                    break;
+                case AGOSTO:
+                    return true;
+                    break;
+                default:
+                    return false;
+                    break;
+            }
+        }
+        public String toString(){
+            System.out.println(getDia + " de " + getMes + " del " + getAnio);
+        }
+        
     }
 }
