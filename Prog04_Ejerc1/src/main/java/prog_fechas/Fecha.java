f83e2ebe3bb0b0c4b7087ab1c12c31a4ae4a6896
Fluna29 <fluna293107@gmail.com>
2023-11-19 19:04:15 +0100

    Tarea 4 Ejercicio 1 Completado y Comentado

--- Prog04_Ejerc1/src/main/java/prog_fechas/Fecha.java
+++ Prog04_Ejerc1/src/main/java/prog_fechas/Fecha.java
@@ -10,24 +10,29 @@ package prog_fechas;
  * @author Francisco Luna
  */
 public class Fecha {
+    /**
+     * Creamos un enumerador con los meses del año, para usarlo en el constructor.
+     * @param enumMes
+     */
     public enum enumMes {
-            enero, 
-            febrero, 
-            marzo,
-            abril,
-            mayo,
-            junio,
-            julio,
-            agosto, 
-            septiembre,
-            octubre,
-            noviembre,
-            diciembre,
+            Enero, 
+            Febrero, 
+            Marzo,
+            Abril,
+            Mayo,
+            Junio,
+            Julio,
+            Agosto, 
+            Septiembre,
+            Octubre,
+            Noviembre,
+            Diciembre,
         }
 
     private int dia;
     private enumMes mes;
     private int anio;
+
     /**
      * Aquí creamos un constructor llamado "fechas". con las variables creadas anteriormente
      * llamadas "dia", "mes" y "anio"
@@ -35,27 +40,50 @@ public class Fecha {
      * @param mes
      * @param anio 
      */
-    
     public void fechas(int dia, enumMes mes, int anio){
         this.dia = dia;
         this.mes = mes;
         this.anio = anio;
-    }   
+    }
+    /**
+     * Getter del parámetro día.
+     * @param dia
+    */   
     public int getDia(){
         return dia;
     }
+    /**
+     * Setter del parametro día
+     * @param dia
+     */
     public void setDia(int dia){
         this.dia = dia;
     }
+    /**
+     * Getter del parámetro mes
+     * @param mes
+     */
     public enumMes getMes(){
         return mes;
     }
+    /**
+     * Setter del parámetro mes
+     * @param mes
+     */
     public void setMes(enumMes mes){
         this.mes = mes;
     }
+    /**
+     * Getter del parámetro anio
+     * @param anio
+     */
     public int getAnio(){
         return anio;
     }
+    /**
+     * Setter del parámetro anio
+     * @param anio
+     */
     public void setAnio(int anio){
         this.anio = anio;
     }
@@ -63,12 +91,20 @@ public class Fecha {
     /**
     * Esta función es un boolean, que nos dice si ese mes está situado en verano o no, para ello hace uso
     * del boolean.
+    * Retorna true o false, dependendiendo de si es verano o no.
     */
-    
     public boolean isSummer(){
-        return (mes == enumMes.junio || mes == enumMes.julio || mes == enumMes.agosto);
+        return (mes == enumMes.Junio || mes == enumMes.Julio || mes == enumMes.Agosto);
     }
     
+    /**
+     * En esta función convertimos el completo del mensaje y valores a String, 
+     * mediante el uso de la función toString.
+     * 
+     * Retorna el siguiente mensaje (NºDia) de (mes) del (año), 
+     * ejemplo: 15 de Enero de 2020
+     * 
+     */
     public String toString(){
         return (this.getDia()+ " de " + this.getMes() + " del " + getAnio());
     }
--- Prog04_Ejerc1/src/main/java/prog_fechas/Principal.java
+++ Prog04_Ejerc1/src/main/java/prog_fechas/Principal.java
@@ -1,19 +1,34 @@
 package prog_fechas;
 import prog_fechas.Fecha.enumMes;
+
 /**
- * En esta clase incorporamos el método main, para poder así ejecutar nuestros objetos y nuestro programa
+ * En esta clase incorporamos el método main, para poder así crear nuestros objetos
+ * y ejecutar nuestro programa incorporando las funciones creadas en el archivo Fecha.java
  * @author fluna
  */
-
 public class Principal {
 
+    /**
+     * Esta función da como retorno los siguientes mensajes, después de haber creado nuestros dos objetos:
+     * 
+     * Primera fecha, inicializada con el primer constructor
+     * La fecha es: 20 de Febrero del 2000
+     * No es verano
+     * 
+     * Segunda fecha, inicializada con el segundo constructor
+     * La fecha 2 contiene el año 2015
+     * La fecha es: 15 de Julio del 2015
+     * Es verano
+     * @param args
+     */
     public static void main (String[] args){
+        //Creamos el primer objeto con el nombre "objFecha1"
         prog_fechas.Fecha objFecha1 = new prog_fechas.Fecha();
-
-        objFecha1.setMes(enumMes.febrero);
+        //Aquí cambiamos los atributos del primer objeto
+        objFecha1.setMes(enumMes.Febrero);
         objFecha1.setDia(20);
         objFecha1.setAnio(2000);
-
+        //Enviamos el primer mensaje
         System.out.println("\nPrimera fecha, inicializada con el primer constructor");
         System.out.println("La fecha es: " + objFecha1.toString());
         if (objFecha1.isSummer()) {
@@ -23,12 +38,13 @@ public class Principal {
             System.out.println("No es verano");
         }
 
+        //Creamos el segundo objeto con el nombre "objFecha2"
         prog_fechas.Fecha objFecha2 = new prog_fechas.Fecha();
-
-        objFecha2.setMes(enumMes.julio);
+        //Cambiamos los atributos del segundo objeto
+        objFecha2.setMes(enumMes.Julio);
         objFecha2.setDia(15);
         objFecha2.setAnio(2015);
-
+        //Enviamos el segundo mensaje
         System.out.println("\nSegunda fecha, inicializada con el segundo constructor");
         System.out.println("La fecha 2 contiene el año " + objFecha2.getAnio());
         System.out.println("La fecha es: " + objFecha2.toString());
